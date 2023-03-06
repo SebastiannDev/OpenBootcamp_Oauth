e61f6dfc42a117b3de1cbfc09a5a0bc48cee5e08
sebastian <sebastiajhoan72@outlook.es>
2023-03-05 19:47:15 -0500

    first commit

---
+++ src/main/java/com/open_bootcamp/oauth_course/course_oauth/CourseOauthApplication.java
@@ -0,0 +1,13 @@
+package com.open_bootcamp.oauth_course.course_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/open_bootcamp/oauth_course/course_oauth/CourseOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open_bootcamp.oauth_course.course_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
